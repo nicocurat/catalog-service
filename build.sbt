e04d047a6a8c1030db8daca0d0d1c653858f8a12
nicocurat <nicurat@gmail.com>
2019-03-31 16:46:30 -0300

    Start catalog service project

---
+++ build.sbt
@@ -0,0 +1,23 @@
+import Dependencies._
+
+ThisBuild / scalaVersion     := "2.12.8"
+ThisBuild / version          := "0.1.0-SNAPSHOT"
+ThisBuild / organization     := "com.example"
+ThisBuild / organizationName := "example"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "catalog-service",
+    libraryDependencies += scalaTest % Test
+  )
+
+// See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
+libraryDependencies ++= Seq(
+  "io.grpc" % "grpc-netty" % scalapb.compiler.Version.grpcJavaVersion,
+  "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion,
+  "com.thesamet.scalapb" %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion % "protobuf"
+)
+
+PB.targets in Compile := Seq(
+  scalapb.gen() -> (sourceManaged in Compile).value
+)
---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,51 @@
+import catalog.product.CatalogServiceGrpc.CatalogService
+import catalog.product._
+import catalog.user.{AddItemRequest, AddItemResponse, User}
+import catalog.user.UserServiceGrpc.UserService
+
+import scala.concurrent.Future
+
+object Demo extends App {
+
+  val ipad = Product(0, "Ipad", "Apple Ipad 32 Gb")
+  val mac = Product(0, "Macbook Pro", "Of course")
+  val pixel = Product(0, "Google Pixel", "Better than the shitty iPhone")
+
+  val products = Vector(ipad, mac, pixel)
+
+  val catalog = Catalog(0, "What you need!", products)
+
+  val curi = User(0, "Nicolás", "Curi", products)
+
+  val service = new CurisService()
+
+  val catalogService = new ProductCatalogService(catalog)
+
+}
+
+class CurisService extends UserService {
+  override def addItem(request: AddItemRequest): Future[AddItemResponse] = {
+    request.user map { user: User =>
+      request.product map { product: Product =>
+        user.addProducts(product)
+        Future.successful(AddItemResponse(s"Product Added to ${user.username} list"))
+      }
+    } getOrElse Future.successful(AddItemResponse("Hey there"))
+    Future.successful(AddItemResponse(s"Product Added to list"))
+  }
+}
+
+class ProductCatalogService(catalog: Catalog) extends CatalogService {
+
+  override def getProduct(request: ProductIdRequest): Future[Product] = {
+    val product = catalog.products.find(product => product.id == request.productId)
+    product map (product => return Future.successful(product))
+    Future.successful(null)
+  }
+
+  override def getProductsFromCatalog(request: CatalogIdRequest): Future[ProductsList] = {
+    Future.successful(ProductsList(catalog.products))
+  }
+
+  override def getAllProducts(request: None): Future[ProductsList] = Future.successful(ProductsList(catalog.products))
+}
