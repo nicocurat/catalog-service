b6dbe9f8aa3bb306efd97b5e54552f22569c6c6f
Tomas Piaggio <tomas.piaggio12@gmail.com>
2019-04-04 18:49:48 -0300

    I dont know what i changed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,8 +5,8 @@ object Main extends App {
   import scala.concurrent.ExecutionContext.Implicits.global
 
   for {
-    first <- ProductStorageService.insert(Product(1, "Revista", "Para cuando cagas"))
-    second <- ProductStorageService.getProductById(first id)
+    first <- ProductStorageService insert Product(1, "Revista", "Para cuando cagas")
+    second <- ProductStorageService getProductById (first id)
   } yield println(second)
 
 
