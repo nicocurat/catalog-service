52718ea5e14c3acab8fe56c2f3df8c9bb4014792
Tomas Piaggio <tomas.piaggio12@gmail.com>
2019-04-07 19:28:21 -0300

    Continued implementation of UserStorageService

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,34 @@
+import java.util.concurrent.Executors
+
 import catalog.product.Product
+import catalog.user.User
+
+import scala.concurrent.ExecutionContext
 
 object Main extends App {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  implicit val exec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(16))
 
   for {
-    first <- ProductStorageService insert Product(1, "Revista", "Para cuando cagas")
-    second <- ProductStorageService getProductById (first id)
-  } yield println(second)
+    first <- ProductStorageService insert Product(0, "Revista", "Para cuando cagas")
+    second <- ProductStorageService insert Product(1, "Papel higienico", "Para limpiar la revista")
+    third <- ProductStorageService insert Product(2, "Esponja anal", "Para cuando rompes culos")
+    user <- UserStorageService insert User(0, "tomas", "piaggio", List(first, second, third))
+    last <- UserStorageService getUserById (user id)
+  } yield println(third)
 
+//  val before1 = System.currentTimeMillis()
+//  UserStorageService.getUserById(0).map(result => {
+//    println(s"Fetched user and products in ${System.currentTimeMillis() - before1}")
+//    println(s"Result $result")
+//  })
+//
+//  val before2 = System.currentTimeMillis()
+//  ProductStorageService.getProductById(0).map(result => {
+//    println(s"Fetched products in ${System.currentTimeMillis() - before2}")
+//    println(s"Result $result")
+//  })
 
-  Thread.sleep(1000)
+  println("Enter to exit")
+  System.in.read
 }
--- src/main/scala/services/UserStorageService.scala
+++ src/main/scala/services/UserStorageService.scala
@@ -12,8 +12,21 @@ object UserStorageService extends StorageService("user") {
     .first
     .toFuture
     .map(MongoUser from)
+    .map(fillProducts)
+    .flatten
 
-  def insert(user: User): Future[Completed] = collection
+  def getAll: Future[Seq[User]] = collection
+    .find
+    .toFuture
+    .map(_ map(MongoUser from))
+    .map(users => Future sequence (users map fillProducts))
+    .flatten
+
+  def insert(user: User): Future[User] = collection
     .insertOne(MongoUser from user)
     .toFuture
+    .map(_ => user)
+
+  private def fillProducts(user: User): Future[User] = Future.sequence(user.products.map(prod => ProductStorageService getProductById(prod id)))
+    .map(prods => User(user.id, user.name, user.username, prods))
 }
