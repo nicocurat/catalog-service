91e5a69273da318a0fb27d2bf41e77a0fcdf60dc
nicocurat <nicurat@gmail.com>
2019-05-16 01:00:45 -0300

    minor fixes

--- wishlist-service/src/main/scala/services/StorageService.scala
+++ wishlist-service/src/main/scala/services/StorageService.scala
@@ -6,7 +6,7 @@ import scala.concurrent.ExecutionContext
 
 abstract class StorageService(private val collectionName: String) {
     private lazy val mongoClient: MongoClient = MongoClient()
-    protected lazy val database: MongoDatabase = mongoClient.getDatabase("distribuidos-mongo")
+    protected lazy val database: MongoDatabase = mongoClient.getDatabase("catalogDB")
     private lazy val collectionObject = database.getCollection(collectionName)
     protected implicit val exec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
