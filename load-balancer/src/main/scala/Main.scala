29e89e2ebe91af79a21e61dc836658e562c5be4f
nicocurat <nicurat@gmail.com>
2019-05-16 17:21:11 -0300

    added user and wishlist ports

--- load-balancer/src/main/scala/Main.scala
+++ load-balancer/src/main/scala/Main.scala
@@ -2,12 +2,12 @@
 object Main extends App {
 
 
-  val userList = List(("localhost", 1000), ("127.0.0.1", 2000), ("127.30.30.23", 3000))
-  val productList = List(("localhost", 4000), ("127.0.0.1", 5000), ("127.30.30.23", 6000))
+  val userList = List(("localhost", 9091))
+  val productList = List(("localhost", 8081))
   val router: Router = new Router(userList, productList)
 
   val interval = 3000
-  println("Empezando")
+  println("Starting")
 
   val runnable: Runnable = () => {
     while (true) {
